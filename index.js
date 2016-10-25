d880d23c4667242d81b9caa6f53495512a117d7a
Airyque Ervin <airyque@gmail.com>
2016-10-25 06:21:22 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector()
+}
+
+// function increaseRankBy(n) {
+//
+// }
+//
+// function deepestChild() {
+//
+// }
